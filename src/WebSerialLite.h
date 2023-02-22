86ae4c640f3efeec1d34702fef0e9b169f6246ca
nullstalgia <nullstalgia@gmail.com>
2023-02-21 19:49:41 -0800

    Properly inherit from Print

--- src/WebSerialLite.h
+++ src/WebSerialLite.h
@@ -31,7 +31,7 @@ typedef std::function<void(AsyncWebSocketClient *, uint16_t code,
 // Uncomment to enable webserial debug mode
 // #define WEBSERIAL_DEBUG
 
-class WebSerialClass {
+class WebSerialClass : public Print {
  public:
   void begin(AsyncWebServer *server, const char *url = "/webserial");
 
@@ -43,8 +43,43 @@ class WebSerialClass {
 
   // Print
 
-  size_t write(uint8_t);
-  size_t write(const uint8_t* buffer, size_t size);
+  void print(String m = "");
+
+  void print(const char *m);
+
+  void print(char *m);
+
+  void print(int m);
+
+  void print(uint8_t m);
+
+  void print(uint16_t m);
+
+  void print(uint32_t m);
+
+  void print(double m);
+
+  void print(float m);
+
+  // Print with New Line
+
+  void println(String m = "");
+
+  void println(const char *m);
+
+  void println(char *m);
+
+  void println(int m);
+
+  void println(uint8_t m);
+
+  void println(uint16_t m);
+
+  void println(uint32_t m);
+
+  void println(float m);
+
+  void println(double m);
 
  private:
   AsyncWebServer *_server;
