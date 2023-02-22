b1fbb912df23069b45e88ca93cec6cff8d0dfb92
杨成锴 <homeboyc@foxmail.com>
2023-02-22 23:03:51 +0800

    remove redundant space

--- src/WebSerialLite.h
+++ src/WebSerialLite.h
@@ -41,7 +41,7 @@ class WebSerialClass : public Print {
   void onMessage(RecvMsgHandlerPlus callbackFunc);
   void onError(ErrHandler callbackFunc);
 
-   // Print
+  // Print
 
   size_t write(uint8_t);
   size_t write(const uint8_t* buffer, size_t size);
