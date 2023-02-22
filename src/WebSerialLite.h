758a1732eb621273457987fbc27e3d93e54e97bf
nullstalgia <nullstalgia@gmail.com>
2023-02-22 07:27:25 -0800

    feat: inherit from Print instead of overloading print and fix crash
    * Update WebSerial.cpp
    
    * Update WebSerialLite.h
    
    * Only send if the websocket exists
    
    * Properly inherit from Print
    
    * Update demos with examples of new functionality
    
    * Accidentally undid the changes in the Header
    
    * remove redundant space
    
    * update: fix build in esp32
    
    * update: fix build in esp32
    
    ---------
    
    Co-authored-by: 杨成锴 <homeboyc@foxmail.com>

--- src/WebSerialLite.h
+++ src/WebSerialLite.h
@@ -31,7 +31,7 @@ typedef std::function<void(AsyncWebSocketClient *, uint16_t code,
 // Uncomment to enable webserial debug mode
 // #define WEBSERIAL_DEBUG
 
-class WebSerialClass {
+class WebSerialClass : public Print {
  public:
   void begin(AsyncWebServer *server, const char *url = "/webserial");
 
@@ -43,43 +43,8 @@ class WebSerialClass {
 
   // Print
 
-  void print(String m = "");
-
-  void print(const char *m);
-
-  void print(char *m);
-
-  void print(int m);
-
-  void print(uint8_t m);
-
-  void print(uint16_t m);
-
-  void print(uint32_t m);
-
-  void print(double m);
-
-  void print(float m);
-
-  // Print with New Line
-
-  void println(String m = "");
-
-  void println(const char *m);
-
-  void println(char *m);
-
-  void println(int m);
-
-  void println(uint8_t m);
-
-  void println(uint16_t m);
-
-  void println(uint32_t m);
-
-  void println(float m);
-
-  void println(double m);
+  size_t write(uint8_t);
+  size_t write(const uint8_t* buffer, size_t size);
 
  private:
   AsyncWebServer *_server;
