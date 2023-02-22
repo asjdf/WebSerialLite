8c2068568175eaaa08ed1ba0dd748db9373779ac
nullstalgia <nullstalgia@gmail.com>
2023-02-21 19:42:27 -0800

    Update WebSerialLite.h

--- src/WebSerialLite.h
+++ src/WebSerialLite.h
@@ -43,43 +43,8 @@ class WebSerialClass {
 
   // Print
 
-  void print(String m = "");
-
-  void print(const char *m);
-
-  void print(char *m);
-
-  void print(int m);
-
-  void print(uint8_t m);
-
-  void print(uint16_t m);
-
-  void print(uint32_t m);
-
-  void print(double m);
-
-  void print(float m);
-
-  // Print with New Line
-
-  void println(String m = "");
-
-  void println(const char *m);
-
-  void println(char *m);
-
-  void println(int m);
-
-  void println(uint8_t m);
-
-  void println(uint16_t m);
-
-  void println(uint32_t m);
-
-  void println(float m);
-
-  void println(double m);
+  size_t write(uint8_t);
+  size_t write(const uint8_t* buffer, size_t size);
 
  private:
   AsyncWebServer *_server;
